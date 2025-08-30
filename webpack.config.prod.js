7939df708e1df7baebfdfd282ffc6078fa7072b4
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-08-30 14:54:34 +0300

    added fonts

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,12 +12,6 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'assets', to: 'assets' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
   ],
